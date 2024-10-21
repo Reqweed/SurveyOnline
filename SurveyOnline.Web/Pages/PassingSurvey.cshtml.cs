96601ad99b55084e5b6260f71ee18d02ca1a1f6e
Sergey Vasiliev <aragamerek@gmail.com>
2024-10-21 19:36:32 +0300

    feat: add page for completed survey

---
+++ SurveyOnline.Web/Pages/PassingSurvey.cshtml.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SurveyOnline.BLL.Entities.DTOs.Survey;
+using SurveyOnline.BLL.Services.Contracts;
+
+namespace SurveyOnline.Web.Pages;
+
+[Authorize]
+public class PassingSurvey(IServiceManager serviceManager) : PageModel
+{
+    [BindProperty]
+    public SurveyForCompletedDto Survey { get; set; }
+
+    [BindProperty]
+    public List<string> Answers { get; set; }
+    
+    public async Task OnGet([FromQuery] Guid id)
+    {
+        Survey = await serviceManager.Survey.GetSurveyAsync(id);
+        Answers = new List<string>(Survey.Questions.Count);
+    }
+    
+    public async Task<IActionResult> OnPostAsync()
+    {
+        await serviceManager.CompletedSurvey.AddCompletedSurvey(Survey, Answers);
+        
+        return RedirectToPage(nameof(Index));
+    }
+}
